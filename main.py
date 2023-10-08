4a7e3f8d4fd32357cc40bb1fb47f1608fda5af1e
ren-joey <tsuyiren@gmail.com>
2023-10-09 05:47:22 +0800

    docs: ✏️ add references in readme file

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import uvicorn
-from fastapi import FastAPI
+import time
+import socket
+from sklearn.datasets import load_iris
 
-app = FastAPI()
+data = load_iris()
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('0.0.0.0', 9999))
+server.listen()
 
-@app.get('/')
-def central_function():
-    return {"Neural": "Nine"}
-
-
-if __name__ == '__main__':
-    uvicorn.run(app, port=8000, host='0.0.0.0')
+while True:
+    client, addr = server.accept()
+    print('Connection from', addr)
+    client.send('You are connected!\n'.encode())
+    client.send(f"{data['data'][:, 0]}\n".encode())
+    time.sleep(2)
+    client.send('You are being disconnected!\n'.encode())
+    client.close()
